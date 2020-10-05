909e8567e820d07c40b6a0d2bc7d1bb041db8968
Javier Castillo Castellanos <javier.castillo.castellanos@cern.ch>
2020-10-05 22:41:30 +0200

    Remove error of loading libAliPythia6.so

--- ConvertMCHESDTracks.C
+++ ConvertMCHESDTracks.C
@@ -1,56 +1,59 @@
 #include <iostream>
 // MUON includes
-#include "TFile.h"
-#include "TTree.h"
 #include "AliESDEvent.h"
 #include "AliRunLoader.h"
 #include "AliStack.h"
+#include "TFile.h"
+#include "TTree.h"
 #include "include/tempMCHTrackGetter.h"
 
+#if !defined(__CLING__) || defined(__ROOTCLING__)
+R__LOAD_LIBRARY(libpythia6_4_25)
+#endif
 
 using namespace std;
 
-
-#pragma link C++ class tempMCHTrack+;
-#pragma link C++ class std::vector<tempMCHTrack>+;
+#pragma link C++ class tempMCHTrack + ;
+#pragma link C++ class std::vector < tempMCHTrack> + ;
 
 std::vector<tempMCHTrack> tempMCHTracks;
 
-
 Int_t fNMCH;
 
-TTree* treeMCH = new TTree("treeMCH","treeMCH");
+TTree *treeMCH = new TTree("treeMCH", "treeMCH");
 
-
-void ConvertMCHESDTracks(string in_dir=""){
-  //gSystem->Load("libpythia6_4_25");
+void ConvertMCHESDTracks(string in_dir = "") {
+  // gSystem->Load("libpythia6_4_25"); // Why is this needed here? To remove
+  //                                   // libAliPythia6 errors
 
   string out_dir = in_dir;
-  if(!TFile::Open(Form("%s/AliESDs.root",in_dir.c_str()))) return;
+  if (!TFile::Open(Form("%s/AliESDs.root", in_dir.c_str())))
+    return;
 
-  TFile* inFile = new TFile(Form("%s/AliESDs.root",in_dir.c_str()),"read");
+  TFile *inFile = new TFile(Form("%s/AliESDs.root", in_dir.c_str()), "read");
 
-  TTree *esdTree = (TTree*)inFile->Get("esdTree");
+  TTree *esdTree = (TTree *)inFile->Get("esdTree");
 
   AliESDEvent *ev = new AliESDEvent();
   ev->ReadFromTree(esdTree);
 
-  AliRunLoader* rl = AliRunLoader::Open(Form("%s/galice.root",in_dir.c_str()));
+  AliRunLoader *rl = AliRunLoader::Open(Form("%s/galice.root", in_dir.c_str()));
   rl->LoadgAlice();
-  //gAlice = rl->GetAliRun();
+  // gAlice = rl->GetAliRun();
   rl->LoadKinematics();
   rl->LoadHeader();
 
-  treeMCH->Branch("tempMCHTracks",&tempMCHTracks);
+  treeMCH->Branch("tempMCHTracks", &tempMCHTracks);
 
-  TFile* output  = new TFile(Form("%s/tempMCHTracks.root",out_dir.c_str()),"recreate");
+  TFile *output =
+      new TFile(Form("%s/tempMCHTracks.root", out_dir.c_str()), "recreate");
 
   Long64_t nEvents = rl->GetNumberOfEvents();
 
   for (Int_t iEv = 0; iEv < nEvents; iEv++) {
 
     std::cout << "#Evt: " << iEv << std::endl;
-    fNMCH=0;
+    fNMCH = 0;
 
     tempMCHTracks.clear();
 
@@ -59,20 +62,25 @@ void ConvertMCHESDTracks(string in_dir=""){
 
     Int_t nTracks = ev->GetNumberOfMuonTracks();
 
-    AliStack * stack = rl->Stack();
-    //Int_t nPart = stack->GetNtrack();
+    AliStack *stack = rl->Stack();
+    // Int_t nPart = stack->GetNtrack();
 
     for (Int_t iTr = 0; iTr < nTracks; iTr++) {
-      AliESDMuonTrack* esdTrack = (AliESDMuonTrack*)ev->GetMuonTrack(iTr);
+      AliESDMuonTrack *esdTrack = (AliESDMuonTrack *)ev->GetMuonTrack(iTr);
 
-      if (!esdTrack->ContainTrackerData() || esdTrack->GetLabel()<0) continue;
+      if (!esdTrack->ContainTrackerData() || esdTrack->GetLabel() < 0)
+        continue;
 
-      TParticle * part = stack->Particle(esdTrack->GetLabel());
+      TParticle *part = stack->Particle(esdTrack->GetLabel());
 
-      if(esdTrack->GetMatchTrigger()<1)                                      continue;
-      //if(esdTrack->GetChi2() / esdTrack->GetNDF()>5.0)                             continue;
-      if(-3.6>esdTrack->Eta() && esdTrack->Eta()>-2.1)                             continue;
-      if(17.6>esdTrack->GetRAtAbsorberEnd() && esdTrack->GetRAtAbsorberEnd()>89.5) continue;
+      if (esdTrack->GetMatchTrigger() < 1)
+        continue;
+      // if(esdTrack->GetChi2() / esdTrack->GetNDF()>5.0) continue;
+      if (-3.6 > esdTrack->Eta() && esdTrack->Eta() > -2.1)
+        continue;
+      if (17.6 > esdTrack->GetRAtAbsorberEnd() &&
+          esdTrack->GetRAtAbsorberEnd() > 89.5)
+        continue;
 
       tempMCHTrackGetter t(*esdTrack);
       tempMCHTrack thisTrack;
@@ -84,7 +92,6 @@ void ConvertMCHESDTracks(string in_dir=""){
     }
 
     treeMCH->Fill();
-
   }
 
   output->WriteTObject(treeMCH);
--- generators/rec.C
+++ generators/rec.C
@@ -1,12 +1,14 @@
 void rec() {
+  gSystem->Load("libpythia6_4_25"); // Why is this needed here? To remove
+                                    // libAliPythia6 errors
+
   AliReconstruction reco;
 
   reco.SetWriteESDfriend();
   reco.SetWriteAlignmentData();
 
   reco.SetDefaultStorage("local://$ALIROOT_OCDB_ROOT/OCDB");
-  reco.SetSpecificStorage("GRP/GRP/Data",
-			  Form("local://%s",gSystem->pwd()));
+  reco.SetSpecificStorage("GRP/GRP/Data", Form("local://%s", gSystem->pwd()));
   reco.SetRunPlaneEff(kTRUE);
 
   reco.SetFractionFriends(1.);
