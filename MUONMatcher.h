e2d977ffa0fecb458ad700b6924c1cdf92d78bea
LucasNL <nunes.lopes@ufrgs.br>
2021-05-11 17:49:27 +0000

    Fix bug preventing from passing correctly the training string

--- MUONMatcher.h
+++ MUONMatcher.h
@@ -290,12 +290,14 @@ class MUONMatcher
 
     std::string training_string("");
     if (network_ID != "") {
-      std::string training_string(opt_reader());
+      training_string = opt_reader();
     } else {
       std::cout << " [WARNING] Configurations for ML method were not setted. I'll use TMVAs default; hope that works!" << endl;
     }
 
     std::cout << " Network name: " << network_ID << "\n"
+              << std::endl;
+    std::cout << " Complete Option String: " << training_string << "\n"
               << std::endl;
 
     std::string MLAnalysisType = gSystem->Getenv("ML_TYPE");
