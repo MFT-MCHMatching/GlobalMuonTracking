55b02b364dbcb039ffe7169a0595b09900919b76
syano0822 <32352856+syano0822@users.noreply.github.com>
2021-06-30 16:03:47 +0900

    Update Dimuon Generator (#24)
    * Add Hiroshima score method
    
    * Apply clang-format
    
    * Enable consistent use of the Hiroshima matching function
    
     - Function name added to MatchingHelper
     - Added function to matcher.sh usage help
     - rename class function for consistency with other match functions
    
    * Modify the python training part
    
    * Fix the bug in AliGenDimuon
    
    * Fix the bug in AliGenDimuon and add inout file for the generator
    
    * Clean generators

--- generators/AliGenDimuon.h
+++ generators/AliGenDimuon.h
@@ -30,26 +30,53 @@ public:
 
   void SetGenerateParticle(Int_t code){fPdgCode = code;}
   
+  Bool_t SetMuonMomentumRange(Double_t min, Double_t max){
+    kMuonMomentumRange = kTRUE;
+    fMinMuonMomentum   = min;
+    fMaxMuonMomentum   = max;
+  }
+
+  Bool_t SetMuonRapRange(Double_t min, Double_t max){
+    kMuonRapRange = kTRUE;
+    fMinMuonRap   = min;
+    fMaxMuonRap   = max;
+  }
+
+  Bool_t SetMuonEtaRange(Double_t min, Double_t max){
+    kMuonEtaRange = kTRUE;
+    fMinMuonEta   = min;
+    fMaxMuonEta   = max;
+  }
+
 private:
 
   AliGenDimuon(const AliGenDimuon&);
   AliGenDimuon &operator=(const AliGenDimuon&);
 
-  void SetPtShape();
-  void SetRapidityShape();
-
   Int_t fPdgCode;
+  
+  Double_t fMinMuonMomentum;
+  Double_t fMaxMuonMomentum;
+
+  Double_t fMinMuonRap;
+  Double_t fMaxMuonRap;
+
+  Double_t fMinMuonEta;
+  Double_t fMaxMuonEta;
+  
+  Bool_t kMuonMomentumRange;
+  Bool_t kMuonRapRange;
+  Bool_t kMuonEtaRange;
+  
+  TH2F* fHistParentKine;
+  
+  Double_t EtaToTheta(Double_t arg);
 
 protected:
-
-  TF1* fPt;
-  TF1* fRap;
 
   //enum E_Parent{Rho2Body,Omega2Body,Phi2Body,Jpsi2Body,Psiprime,Upsilon1S,Upsilon2S,Upsilon3S,BtoJpasi,LMRinMedium};
 
   Double_t mass[10];
-
-  TF1* fBW;
   
   ClassDef(AliGenDimuon, 1)
 
