b0ffd7001f933e0bb6ebeb0653f1736ce632930b
Rafael Pezzi <rafael.pezzi@cern.ch>
2022-01-21 12:06:22 +0100

    Adapt macro to finalize global forward assessment workflow
    Depends on https://github.com/AliceO2Group/AliceO2/pull/7982

--- finalizeGlobalFwdAssessment.C
+++ finalizeGlobalFwdAssessment.C
@@ -1,13 +1,11 @@
-#include "GlobalTracking/MatchGlobalFwdAssessment.h"
-
-// Dictionaries for MatchGlobalFwdAssessment.h are needed to have this fully working on a macro. 
-// In the meantime one can run the following code on a root shell to finalize assessment
-
-o2::globaltracking::GloFwdAssessment analyser(true);
-analyser.loadHistos(); // loads GlobalForwardAssessment.root produced by the DPL workflow
-analyser.finalizeAnalysis();
-TFile* fout = new TFile("GlobalForwardAssessmentFinalized.root", "RECREATE");
-TObjArray objarOut;
-analyser.getHistos(objarOut);
-objarOut.Write();
-fout->Close();
+void finalizeGlobalFwdAssessment()
+{
+    o2::globaltracking::GloFwdAssessment analyser(true);
+    analyser.loadHistos(); // loads GlobalForwardAssessment.root produced by the DPL workflow
+    analyser.finalizeAnalysis();
+    TFile *fout = new TFile("GlobalForwardAssessmentFinalized.root", "RECREATE");
+    TObjArray objarOut;
+    analyser.getHistos(objarOut);
+    objarOut.Write();
+    fout->Close();
+}
