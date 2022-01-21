b3e4989605e8b4c6ade76f45dec0c8990c0a3cc2
Rafael Pezzi <rafael.pezzi@cern.ch>
2022-01-21 12:06:22 +0100

    Adapt macro to finalize global forward assessment workflow
    Depends on https://github.com/AliceO2Group/AliceO2/pull/7982

--- finalizeMFTAssessment.C
+++ finalizeMFTAssessment.C
@@ -3,7 +3,7 @@ void finalizeMFTAssessment()
   o2::mft::MFTAssessment analyser(true);
   analyser.loadHistos(); // loads MFFAssessment.root produced by the DPL workflow
   analyser.finalizeAnalysis();
-  TFile* fout = new TFile("MFTAssessmentFinalized.root", "RECREATE");
+  TFile *fout = new TFile("MFTAssessmentFinalized.root", "RECREATE");
   TObjArray objarOut;
   analyser.getHistos(objarOut); // Output objects
   objarOut.Write();
