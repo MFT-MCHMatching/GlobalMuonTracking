a47b39b0b8d8e7b13c20d524ac710da6153dc544
Rafael Pezzi <rafael.pezzi@cern.ch>
2022-02-11 12:10:50 +0100

    Add README and organize in folders

--- Assessment/finalizeGlobalFwdAssessment.C
+++ Assessment/finalizeGlobalFwdAssessment.C
@@ -1,7 +1,8 @@
 void finalizeGlobalFwdAssessment()
 {
     o2::globaltracking::GloFwdAssessment analyser(true);
-    analyser.loadHistos(); // loads GlobalForwardAssessment.root produced by the DPL workflow
+    analyser.loadHistos();                     // loads GlobalForwardAssessment.root produced by the DPL workflow
+    analyser.finalizeCutConfig(1.f, 15.f, 15); // finalizeCutConfig(float minCut, float maxCut, int nSteps)
     analyser.finalizeAnalysis();
     TFile *fout = new TFile("GlobalForwardAssessmentFinalized.root", "RECREATE");
     TObjArray objarOut;
